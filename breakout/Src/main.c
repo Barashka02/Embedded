2c5d4e3616e382b98cfe603429a2b6788bf1c4a8
Barashka02 <paulnrunov@gmail.com>
2024-04-21 00:45:12 -0600

    Brickastrophe Averted: Commit Deployed ---Built by Bricking Bad---

--- breakout/Src/main.c
+++ breakout/Src/main.c
@@ -1,3 +1,9 @@
+
+//---------------------------------------------------
+// THIS IS THE MAIN FILE THAT CONTROLS ALL GAME FLOW
+//---------------------------------------------------
+
+
 #include <C8051F020.h>
 #include "../Inc/lcd.h"
 #include "../Inc/draw.h"
@@ -5,36 +11,50 @@
 #include "../Inc/global.h"
 
 //------------------------------------------------------
-//GLOBAL VARIABLES
+//VARIABLES
 //------------------------------------------------------
 
 sbit BUTTON = P2^6;
-
 code unsigned char sine[16] = { 176, 217, 244, 254, 244, 217, 176, 128, 80, 39, 12, 2, 12, 39, 80, 128 };
 unsigned char phase = sizeof(sine)-1;  // Current point in sine to output
             
+//------------------------------------------------------
+//INTERUPTS 
+//------------------------------------------------------
 
+/**
+ * Interrupt Service Routine for generating sound effects.
+ * It plays a sound by toggling the interrupt flag and outputting a sine wave through a DAC.
+ * No parameters, returns nothing.
+ */
 void sound() interrupt 16
 {
-	T4CON = T4CON^0x80;  // clearing the interupt flag
-	DAC0H = sine[phase];
+    T4CON = T4CON ^ 0x80; 	// Toggle the interrupt flag to clear it
+    DAC0H = sine[phase]; 	// Output the current phase of the sine wave to the DAC
 
-    if (phase < sizeof(sine)-1) {
-        phase++;
+    if (phase < sizeof(sine) - 1) {
+        phase++; 			// Increment the phase of the sine wave
     } else if (duration > 0) {
-        phase = 0;
-        duration--;
+        phase = 0;  		// Reset the phase to the start of the sine wave
+        duration--; 		// Decrement the duration for how long the sound should play
     }
-	if(duration == 0)
-	{
-		T4CON = 0x00;
-	}
+    if (duration == 0) {
+        T4CON = 0x00;  		// Disable the timer if the duration is 0, stopping the sound
+    }
 }
 
+
+/**
+ * ADC Interrupt Service Routine for potentiometer readings.
+ * It processes ADC results to calculate the paddle's position.
+ * No parameters, returns nothing.
+ */
 void adc_int() interrupt 15
 {
-	AD0INT = 0;
-	data_out = (ADC0H << 8) | ADC0L;
+	AD0INT = 0;		
+	data_out = (ADC0H << 8) | ADC0L;	//data out of the ADC. After calculations this will be the leftmost position of the paddle
+
+	//data_out is the scaled to fit the paddle in the screen.  Its a value between 0 and the right edge of the screen (88) - paddles size.
 	if(cur_player == 1)
 	{
 		data_out = (data_out * ((88L - paddle_size_1)+1)) >> 12;
@@ -43,37 +63,46 @@ void adc_int() interrupt 15
 	{
 		data_out = (data_out * ((88L - paddle_size_2)+1)) >> 12;
 	}
-	pot_value += data_out;		// Desired range is 0 to max width - paddle size
-	count++;
+	pot_value += data_out;				// Desired range is 0 to max width - paddle size
+	count++;							//increase cound to move through the main loop
 
-	if(count % 7 == 0)
+	if(count % 7 == 0)					//take the average.
 	{
-		//take the average.
-		pot_avg = pot_value >> 3;
-		pot_value = 0;
+		pot_avg = pot_value >> 3;		//divide by 8 (8 samples/8=average)
+		pot_value = 0;					//reset the accumulating value
 	}
 }
+
+//Interupt for timer2 (looping through main loop
 void timer2(void) interrupt 5{
-	TF2 = 0;
-	if(count == speed) {
+	TF2 = 0;				//reset interupt flag
+
+	if(count == speed)		//enough time has passed. Draw everything
+	{
     	count = 0;
-    	move_on = 1; // Will allow the game loop to advance
+    	move_on = 1;		// Will allow the game loop to advance
 	}
-	else {
-    	move_on = 0; // Prevents the game loop from advancing too quickly
+	else 					//time has not yet passed.  Keep waiting
+	{
+    	move_on = 0; 		// Prevents the game loop from advancing too quickly
 	}
 }
 
 
-
+//------------------------------------------------------
+//MAIN
+//------------------------------------------------------
 void main()
 {
+//Inititizling the speeds and paddles size arrays for later
 	char sizes[4] = {8, 12, 16, 24};
 	char speeds[4] = {50, 70, 90, 110};
+
+//Initialization routine
+//-----------------------------------------------------------
 	WDTCN = 0xde;  // disable watchdog
 	WDTCN = 0xad;
 	XBR2 = 0x40;   // enable port output
-
 	OSCXCN = 0x67; // turn on external crystal
 	TMOD = 0x20;   // wait 1ms using T1 mode 2
 	TH1 = -167;    // 2MHz clock, 167 counts - 1ms
@@ -81,13 +110,17 @@ void main()
 	while ( TF1 == 0 ) { }          // wait 1ms
 	while ( !(OSCXCN & 0x80) ) { }  // wait till oscillator stable
 	OSCICN = 8;    // switch over to 22.1184MHz
+
+	//Enabling interupts
 	IE = 0xA0;
 	EIE2 = 0x06;
+
 	//setting up timer 2 (16 bit 10 milisecond auto reload);
 	T2CON = 0X00;
-
-	RCAP2H = -2211 >> 8; // Assign the high byte
-	RCAP2L = -2211; // Assign the low byte
+	
+	//setting up the timer 2 for the speed of the ball
+	RCAP2H = -2211 >> 8;
+	RCAP2L = -2211;
 
 	//setting up adc
 	ADC0CN = 0x8C;
@@ -99,45 +132,47 @@ void main()
 	DAC0CN = 0x9c;
 	RCAP4H = -1;
 	RCAP4L = -144;
-	// Enable flash writes, set necessary flags
-	PSCTL = 0x01; // PSWE = 1, PSEE = 0
 
+	//starting the timers
 	TR2 = 1;
 	T4CON = T4CON & 0x04;
 	CKCON = 0x40;
 
+//Initializing the switches
+//-----------------------------------------------------------
+	
+	//switch for paddle sizes for player 1 (switches 0 and 1)
 	paddle_size_1 = P1 & 0x03;
 	paddle_size_1  = sizes[paddle_size_1];
 
+	//switch for paddle size for player 2 (switches 2 and 3)
 	paddle_size_2 = (P1 & 0x0c) >> 2;
 	paddle_size_2  = sizes[paddle_size_2];
-
+	
+	//switch for speed of the ball (switchs 4 and 5)
 	speed = (P1 & 0x30) >> 4;
 	speed = speeds[speed];
 
+	//swtich for multi player mode (switch 7)
 	multi_player = P1 >> 7;
-
-
-	init_lcd();
-	//draw_borders();
-	//draw_scores(1, 2, 1, 3);
-	//refresh_screen();
 	
+	init_lcd();
 
-	//---------------
+//Main loop
+//-----------------------------------------------------------
+	
 	while(1)
 	{
+		//Starting screen of the game
 		if(end_game != 1)
 		{
 			display_player_ready();
-			//disp_start_game();
 			refresh_screen();
 			while(BUTTON == 1)
 			{}
 			run = 1;
-			//while(P2^6 == 0)
-			//{}
 		}
+		//Main loop for running through the game
 		while(run == 1)
 		{
 			blank_screen();
@@ -145,21 +180,24 @@ void main()
 			draw_paddle();
 			draw_bricks();
 			move_ball();
+			
+			//updating the high scores
 			if(high_score < score1 || high_score < score2)
 			{
-				if(cur_player == 1)
+				if(cur_player == 1)//player 1 high score
 				{
 					high_score = score1;
-					//*((unsigned char xdata *) 0x00ff) = high_score;
 				}
-				else 
+				else //player 2 high score
 				{
 					high_score = score2;
-					//*((unsigned char xdata *) 0x00ff) = high_score;
 				}
 
 			}
+			// updating the scores
 			draw_scores();
+
+			//waiting to draw scores (so that it draws at a consistent speed and doesn't overwrite previous calls)
 			if(run == 1)
 			{
 	 			while(move_on == 0)
--- breakout/Src/move.c
+++ breakout/Src/move.c
@@ -1,3 +1,8 @@
+
+//----------------------------------------------------------------------------
+// THIS IS THE FILE THAT TAKES CARE OF ALL THE MOVEMENT HAPPENING IN THE GAME
+//----------------------------------------------------------------------------
+
 #include <C8051F020.h>
 #include "../Inc/lcd.h"
 #include "../Inc/draw.h"
@@ -5,268 +10,174 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define BRICK_ROWS 11
-#define BRICK_COLS 5
-#define BRICK_WIDTH 7
-#define BRICK_HEIGHT 4
-#define TOP_OFFSET 10 // Assuming bricks start 10 pixels from the top
-#define LEFT_OFFSET 3 // Assuming bricks start 3 pixels from the left side
-#define BRICK_ROWS 11
-#define BRICK_COLS 5
-#define BRICK_WIDTH 7
-#define BRICK_HEIGHT 4
-#define TOP_OFFSET 10 // Assuming bricks start 10 pixels from the top
-#define LEFT_OFFSET 3 // Assuming bricks start 3 pixels from the left side
-
-
-
 //------------------------------------------------------------------
-//SETTING UP BALL 
+// SETTING UP BALL 
 //------------------------------------------------------------------
 
+code unsigned char ball[] = {0x0e, 0x1f, 0x1f, 0x1f, 0x0e};				//array of the ball shape (to draw)
 
-code unsigned char ball[] = {0x0e, 0x1f, 0x1f, 0x1f, 0x0e};
-char draw_ball(int x, int y)
-{	
-	unsigned char row, col, shift, j, hit;
-	int i;
-	
-	//checking for hits
-	//if(x <= 4 || x >= 76 || y <= 2 || y >= 59) return 1;
+/**
+ * Draw the ball on the screen and handle wall collisions.
+ *
+ * @param x The x-coordinate of the ball's center.
+ * @param y The y-coordinate of the ball's center.
+ * @return int Non-zero if the ball hits any screen edges or zero otherwise.
+ */
+char draw_ball(int x, int y) {
+    unsigned char row, col, shift, j, hit;
+    int i;
 
-	if(x < 4 && x_vel < 0) // The ball has hit the right or left wall
-	{
-		x_vel = -x_vel;
+    // Check for boundary collisions and reverse velocity if necessary.
+    if (x < 4 && x_vel < 0) {  // Left wall collision
+        x_vel = -x_vel;
+        RCAP4H = -2354 >> 8;   // D note
+        RCAP4L = -2354;
+        duration = 40;
+        T4CON = T4CON^0x04;
+        return 0;
+    } else if (x > 78 && x_vel > 0) {  // Right wall collision
+        x_vel = -x_vel;
+        RCAP4H = -2354 >> 8;	// D note
+        RCAP4L = -2354;
+        duration = 40;
+        T4CON = T4CON^0x04;
+        return 0;
+    } else if (y < 5 && y_vel < 0) {  // Top wall collision
+        y_vel = -y_vel;
+        RCAP4H = -2354 >> 8;   // D note
+        RCAP4L = -2354;
+        duration = 40;
+        T4CON = T4CON^0x04;
+        return 0;
+    } else if (y > 62) {  // Bottom boundary, game over scenario
+        RCAP4H = -1868 >> 8;   // F# note
+        RCAP4L = -1868;
+        duration = 300;	       // Longer duration to show current game over
+        T4CON = T4CON^0x04;
+        disp_end_game();
+        return 0;
+    }
+    
+    // Calculate position for drawing the ball.
+    col = x - 2;
+    row = y - 2;
+    shift = row % 8;
+    row = row / 8;
+    hit = 0;
 
-		RCAP4H = -2354 >> 8;
-		RCAP4L = -2354;
-		duration = 40;
-		T4CON = T4CON^0x04;
-		return 0;
-	}
-	else if(x > 78 && x_vel > 0) 
-	{
-		x_vel = -x_vel;
+    // Draw ball on the screen, managing potential overflow.
+    for (j = 0, i = row * 128 + col; j < 5; i++, j++) {
+        int mask = (int)ball[j] << shift;
+        hit |= screen[i] & (unsigned char)mask;
+        hit_location = i;
+        screen[i] |= mask;
 
-		RCAP4H = -2354 >> 8;
-		RCAP4L = -2354;
-		duration = 40;
-		T4CON = T4CON^0x04;
-		return 0;
-	}
-	else if(y < 5 && y_vel < 0) 	//The ball has hit the top wall
-	{
-		y_vel = -y_vel;
-
-		RCAP4H = -2354 >> 8;
-		RCAP4L = -2354;
-		duration = 40;
-		T4CON = T4CON^0x04;
-		return 0;
-	}
-	else if(y > 62) //The ball has gone off the botton 
-	{ 
-	//	if(duration == 0)
-	//	{
-	//		while(duration){};
-	
-		RCAP4H = -1868>> 8;
-		RCAP4L = -1868;
-		duration = 300;
-		T4CON = T4CON^0x04;
-		disp_end_game();
-		return 0;
-	}
-	
-	//shifting to measure top left of ball
-	col = x-2;
-	row = y-2;
-	shift = row % 8;
-	row = row / 8;
-	hit = 0;
-
-	//checking and writing the ball (no overflow on page)
-	for(j=0, i=row*128+col; j<5; i++, j++)
-	{
-		int mask = (int)ball[j]<<shift;
-		hit |= screen[i]&(unsigned char)mask;
-		hit_location = i;
-		screen[i] |= mask;
-
-		//checking and writing the ball (overflow on page)
-		if(mask & 0xff00)
-		{
-			hit |= screen[i+128]&(unsigned char)(mask>>8);
-			hit_location = i+128;
-			screen[i+128] |= (unsigned char)(mask >> 8);
-		}
-		
-		}
-		return hit;
+        if (mask & 0xff00) {
+            hit |= screen[i + 128] & (unsigned char)(mask >> 8);
+            hit_location = i + 128;
+            screen[i + 128] |= (unsigned char)(mask >> 8);
+        }
+    }
+    return hit;
 }
 
-
-void move_ball()
-{
-	char hit_code;	
-	int new_x, new_y;
-    int brick_width = 6;
-    int brick_height = 3;
-	int j, k;
-
-	if(cur_player == 1)
-	{
-		if(score1 % 65 == 0 && y_pos > 30)
-		{
-				for (j = 0; j < 13; j++) {
-	        		for (k = 0; k < 5; k++) {
-	            		p1_bricks[j][k] = 1;  // Reset each brick to 1
-	        	}
-	    	}
-		}
-	}	
-	else
-	{
-		if(score2 % 65 == 0 && y_pos > 30)
-		{
-			for (j = 0; j < 13; j++) {
-        		for (k = 0; k < 5; k++) {
-            		p2_bricks[j][k] = 1;  // Reset each brick to 1
-        		}
-    		}
-		}
-	}
-	
-
-
-	//setting new values of x and y;
-	new_x = x_pos + x_vel;
-	new_y = y_pos + y_vel;
-
-	hit_code = draw_ball(new_x, new_y);
-	if(run == 1)
-	{
-		x_pos = new_x;
-		y_pos = new_y;
-	}
-
-	//code to bounch off paddle
-	if(y_pos == 60 && hit_code > 0)
-	{
-
-		char col = x_pos - pot_avg -2;
-		int div; 
-
-		RCAP4H = -3142 >> 8;
-		RCAP4L = -3142;
-		duration = 25;
-		T4CON = T4CON^0x04;
-
-		if(cur_player == 1)
-		{
-			div = paddle_size_1 / 4;
-		}
-		else
-		{
-			div = paddle_size_2 / 4;
-		}	
-		if(col < div || col > 3 * div)
-		{
-			x_vel = 2;
-			y_vel = -1;
-		}
-
-		else
-		{
-			x_vel = 1;
-			y_vel = -2;
-		}
-		if(col < div * 2)
-		{
-			x_vel = -1 * x_vel;
-		}
-	}
 /**
-    if (hit_code && y_pos < 30) { // Bricks are assumed to be within y < 30
-        int x_index = (x_pos - 3) / 6; // Calculate which column of bricks
-        int y_index = (y_pos - 10) / 4; // Calculate which row of bricks
-		
-		if(cur_player == 1)
-		{
-	        if (p1_bricks[x_index][y_index] == 1) { // Check if the brick is active
-	            char collision_type = get_collision_type(x_pos, y_pos, x_index * 6 + 3, y_index * 4 + 10);
-	            if (collision_type == 'L' || collision_type == 'R') 
-				{
-	                x_vel = -x_vel; // Reverse horizontal velocity for side hits
-	            } 
-				else 
-				{
-	                y_vel = -y_vel; // Reverse vertical velocity for top/bottom hits
-	            }
-	            p1_bricks[x_index][y_index] = 0; // Deactivate the brick
-				score1++;
-	        }
-		else{
-			 if (p2_bricks[x_index][y_index] == 1) { // Check if the brick is active
-		            char collision_type = get_collision_type(x_pos, y_pos, x_index * 6 + 3, y_index * 4 + 10);
-		            if (collision_type == 'L' || collision_type == 'R')
-					{
-		                x_vel = -x_vel; // Reverse horizontal velocity for side hits
+ * Update ball position based on velocity and handle brick and paddle collisions.
+ */
+void move_ball() {
+    char hit_code;    
+    int new_x, new_y, j, k;
 
-		            }
-					else 
-					{
-		                y_vel = -y_vel; // Reverse vertical velocity for top/bottom hits
-		            }
-		            p2_bricks[x_index][y_index] = 0; // Deactivate the brick
-					score2++;
-		        }
-			}
-		}		
-   } 
-**/
-	else if (hit_code > 0 && y_pos < 30 && y_pos > 4) {
+    // Check if it's time to reset bricks for either player.
+    if (cur_player == 1) {
+        if (score1 % 65 == 0 && y_pos > 30) {	//redraw the bricks if all bricks have dissapeared for player 1
+            for (j = 0; j < 13; j++) {
+                for (k = 0; k < 5; k++) {
+                    p1_bricks[j][k] = 1;  // Reset each brick to 1
+                }
+            }
+        }
+    } else {
+        if (score2 % 65 == 0 && y_pos > 30) {	//redraw the bricks if all bricks have dissapeared for player 2
+            for (j = 0; j < 13; j++) {
+                for (k = 0; k < 5; k++) {
+                    p2_bricks[j][k] = 1;  // Reset each brick to 1
+                }
+            }
+        }
+    }
 
-	    int x_index = ((x_pos-3) / 6); // Calculate which column of bricks the ball is in
-	    int y_index = ((y_pos-10) / 4); // Calculate which row of bricks the ball is in
-		
-		if(cur_player == 1)
-		{
-		    if (p1_bricks[x_index][y_index] == 1) { // Check if the brick is active
-		        p1_bricks[x_index][y_index] = 0; // Deactivate the brick
+    // Calculate new position based on current velocity.
+    new_x = x_pos + x_vel;
+    new_y = y_pos + y_vel;
 
-					RCAP4H = -1571>> 8;
-					RCAP4L = -1571;
-					duration = 50;
-					T4CON = T4CON^0x04;
-					  // Reset each brick to 1
-		        y_vel = -y_vel; // Reverse the velocity
-				//x_vel = -x_vel;
-				score1++;
-				if(y_index <= 2)
-				{
-					speed = 40;
-				}
-		    }
+    hit_code = draw_ball(new_x, new_y);
+    if (run == 1) {
+        x_pos = new_x;
+        y_pos = new_y;
+    }
+
+    // Handle paddle bounce logic.
+    if (y_pos == 60 && hit_code > 0) {
+        char col = x_pos - pot_avg - 2;
+        int div; 
+
+        RCAP4H = -3142 >> 8;		//A note
+        RCAP4L = -3142;
+        duration = 25;
+        T4CON = T4CON^0x04;
+
+		 // Determine the section of the paddle impacted based on the current player's paddle size.
+		if (cur_player == 1) {
+		    div = paddle_size_1 / 4; // Divide Player 1's paddle into four segments.
+		} else {
+		    div = paddle_size_2 / 4; // Divide Player 2's paddle into four segments.
 		}
-		else
-		{
-		    if (p2_bricks[x_index][y_index] == 1) { // Check if the brick is active
-		        p2_bricks[x_index][y_index] = 0; // Deactivate the brick
-				
-			
-				RCAP4H = -1571>> 8;
-				RCAP4L = -1571;
-				duration = 50;
-				T4CON = T4CON^0x04;
-		        y_vel = -y_vel; // Reverse the velocity
-				//x_vel = -x_vel;
-				score2++;
-				if(y_index <= 2)
-				{
-					speed = 40;
-				}
-		    }
+
+		// Determine the bounce behavior based on the ball's collision point on the paddle.
+		if (col < div || col > 3 * div) { // Hits outer edges of the paddle
+		    // Steeper angle bounce on outer segments
+		    x_vel = 2;  // Increase horizontal velocity
+		    y_vel = -1; // Decrease vertical velocity
+		} else { // Hits inner segments of the paddle
+		    // Shallower angle bounce on inner segments
+		    x_vel = 1;  // Decrease horizontal velocity
+		    y_vel = -2; // Increase vertical velocity
 		}
-	}
+		// Reverse direction if ball hits the left half of the paddle
+		if (col < div * 2) {
+		    x_vel = -1 * x_vel;
+		}
+   } else if (hit_code > 0 && y_pos < 30 && y_pos > 4) {  
+        int x_index = ((x_pos-3) / 6); // Column of bricks
+        int y_index = ((y_pos-10) / 4); // Row of bricks
+        
+        if (cur_player == 1) {
+            if (p1_bricks[x_index][y_index] == 1) { // Check if the brick is active
+                p1_bricks[x_index][y_index] = 0; // Deactivate the brick
+                RCAP4H = -1571 >> 8;
+                RCAP4L = -1571;
+                duration = 50;
+                T4CON = T4CON^0x04;
+                y_vel = -y_vel; // Reverse the velocity
+                score1++;
+                if (y_index <= 2) {
+                    speed = 40;  // Increase game speed
+                }
+            }
+        } else {
+            if (p2_bricks[x_index][y_index] == 1) { // Check if the brick is active
+                p2_bricks[x_index][y_index] = 0; // Deactivate the brick
+                RCAP4H = -1571 >> 8;
+                RCAP4L = -1571;
+                duration = 50;
+                T4CON = T4CON^0x04;
+                y_vel = -y_vel; // Reverse the velocity
+                score2++;
+                if (y_index <= 2) {
+                    speed = 40;  // Increase game speed
+                }
+            }
+        }
+    }
 }
